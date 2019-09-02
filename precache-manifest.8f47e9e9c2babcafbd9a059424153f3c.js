4a28e805c602ccab51139683aacb7cdc11aa065e
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-02 21:35:53 +0200

    Updates

---
+++ precache-manifest.8f47e9e9c2babcafbd9a059424153f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8729c476b11a82f835712acc45de02",
+    "url": "/Food-recognition/index.html"
+  },
+  {
+    "revision": "6d0cf244b0f767824d05",
+    "url": "/Food-recognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "023e0e2f388fb2f5aaa1",
+    "url": "/Food-recognition/static/css/main.e9dade4f.chunk.css"
+  },
+  {
+    "revision": "6d0cf244b0f767824d05",
+    "url": "/Food-recognition/static/js/2.79a25f5d.chunk.js"
+  },
+  {
+    "revision": "023e0e2f388fb2f5aaa1",
+    "url": "/Food-recognition/static/js/main.0a554936.chunk.js"
+  },
+  {
+    "revision": "db15f45f29fe8e49e705",
+    "url": "/Food-recognition/static/js/runtime~main.dcd37c57.js"
+  }
+]);
